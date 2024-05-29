f6e38013117357cf2fb708e57ca6a50ea7625dc9
Jayden Tan <jayden.tan@u.nus.edu>
2024-05-30 01:15:35 +0800

    pushed

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,7 +1,9 @@
 export const useGetUserInfo = () => {
-    const { name, profilePhoto, userID, isAuth } = JSON.parse(
-        localStorage.getItem("auth")
-    );
+    // Get the item from localStorage
+    const auth = localStorage.getItem("auth");
+
+    // If the item exists, parse it; otherwise, use an empty object
+    const { name, profilePhoto, userID, isAuth } = auth ? JSON.parse(auth) : {};
 
     return { name, profilePhoto, userID, isAuth };
 };
