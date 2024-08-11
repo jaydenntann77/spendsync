82b22f457948b5ae42f38d92ff7134449d3e4021
gwenyth <arqhlls88@gmail.com>
2024-08-12 00:54:08 +0800

    hamburgericon, navbar

--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../../config/firebase-config";
 import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faChartBar,
-    faDollarSign,
-    faUsers,
     faSignOutAlt,
-    faMapMarkerAlt, // Import the icon for Nearby
+    faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Taskbar.css";
@@ -17,6 +14,7 @@ import "./Taskbar.css";
 export const Taskbar = () => {
     const { profilePhoto, userID } = useGetUserInfo();
     const navigate = useNavigate();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State for sidebar visibility
 
     const signUserOut = async () => {
         try {
@@ -33,69 +31,71 @@ export const Taskbar = () => {
         return null; // Do not render the Taskbar
     }
 
+    // Toggle sidebar visibility
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    SpendSync
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
-                >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <NavLink
-                                exact
-                                className="nav-link"
-                                activeClassName="active"
-                                to="/expense-tracker"
-                            >
-                                Dashboard
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                activeClassName="active"
-                                to="/add-transaction"
-                            >
-                                Add Transaction
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                activeClassName="active"
-                                to="/groups"
-                            >
-                                Groups
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink
-                                className="nav-link"
-                                activeClassName="active"
-                                to="/nearby"
-                            >
-                                Nearby
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <ul className="navbar-nav ml-auto">
-                        {profilePhoto && (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        SpendSync
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        onClick={toggleSidebar}
+                    >
+                        <FontAwesomeIcon icon={faBars} />
+                    </button>
+                    <div
+                        className={`collapse navbar-collapse ${isSidebarOpen ? 'show' : ''}`}
+                        id="navbarSupportedContent"
+                    >
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
+                                <NavLink
+                                    exact
+                                    className="nav-link"
+                                    activeClassName="active"
+                                    to="/expense-tracker"
+                                >
+                                    Dashboard
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    activeClassName="active"
+                                    to="/add-transaction"
+                                >
+                                    Add Transaction
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    activeClassName="active"
+                                    to="/groups"
+                                >
+                                    Groups
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    className="nav-link"
+                                    activeClassName="active"
+                                    to="/nearby"
+                                >
+                                    Nearby
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <ul className="navbar-nav ml-auto">
+                            {profilePhoto && (
+                                <li className="nav-item">
                                     <img
                                         src={profilePhoto}
                                         alt="Profile"
@@ -103,23 +103,68 @@ export const Taskbar = () => {
                                         width="40"
                                         height="40"
                                     />
+                                </li>
+                            )}
+                            <li className="nav-item">
+                                <button
+                                    className="btn btn-outline-danger"
+                                    onClick={signUserOut}
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faSignOutAlt}
+                                        className="mr-2"
+                                    />{" "}
+                                    Sign Out
+                                </button>
                             </li>
-                        )}
-                        <li className="nav-item">
-                            <button
-                                className="btn btn-outline-danger"
-                                onClick={signUserOut}
-                            >
-                                <FontAwesomeIcon
-                                    icon={faSignOutAlt}
-                                    className="mr-2"
-                                />{" "}
-                                Sign Out
-                            </button>
-                        </li>
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
+            </nav>
+
+            {/* Sidebar for mobile */}
+            <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                <ul>
+                    <li>
+                        <NavLink
+                            exact
+                            to="/expense-tracker"
+                            onClick={toggleSidebar}
+                        >
+                            Dashboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/add-transaction"
+                            onClick={toggleSidebar}
+                        >
+                            Add Transaction
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/groups"
+                            onClick={toggleSidebar}
+                        >
+                            Groups
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/nearby"
+                            onClick={toggleSidebar}
+                        >
+                            Nearby
+                        </NavLink>
+                    </li>
+                    <li>
+                        <button onClick={signUserOut}>
+                            Sign Out
+                        </button>
+                    </li>
+                </ul>
             </div>
-        </nav>
+        </>
     );
 };
